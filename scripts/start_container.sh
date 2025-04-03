2f164bad9a4cc0163c79bb0c4ace74cbcce6821c
mvirk2 <mansiratvirk98@gmail.com>
2025-04-03 01:40:00 -0400

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
   set -e
 
   # Pull the latest Docker image from Docker Hub
-  docker pull mansiratvirk98/tws-aws-cicd-demo:latest
+  docker pull mansiratvirk98/mansiratdevops:latest
 
   # Run the Docker image as a container
-  docker run -d -p 8000:8000 mansiratvirk98/tws-aws-cicd-demo:latest
+  docker run -d -p 8000:8000 mansiratvirk98/mansiratdevops:latest
